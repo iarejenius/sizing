2b25e7176627e72ac4ae29b1ba0a7e2d8c51d18b
Timothy Wittig <iarejenius@gmail.com>
2019-11-04 22:00:45 -0500

    Display participants

--- ClientApp/src/app/session/session.component.ts
+++ ClientApp/src/app/session/session.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SessionService } from '../services/session.service';
 import { Session } from '../models/session';
+import { Participant } from '../models/participant';
 
 @Component({
   selector: 'app-session',
@@ -10,6 +11,7 @@ import { Session } from '../models/session';
 export class SessionComponent implements OnInit {
 
   private key: string;
+  private participants: Participant[] = [];
 
   constructor(private activatedRoute: ActivatedRoute, private sessionService: SessionService) {
 
@@ -31,6 +33,16 @@ export class SessionComponent implements OnInit {
     });
 
     // Set up participant handlers here.
+    this.sessionService.participantJoined.subscribe(participant => {
+      console.log(`${participant.name} joined`);
+      this.participants.push(participant);
+    });
+
+    this.sessionService.participantLeft.subscribe(participant => {
+      console.log(`${participant.name} left`);
+      const index = this.participants.findIndex(p => p.id === participant.id);
+      this.participants.splice(index, 1);
+    });
   }
 
   ngOnInit() { }
---
+++ FEATURES.md
@@ -0,0 +1,27 @@
+# Brainstorming
+## Session
+- Display participants
+    - name
+    - size
+        - whether or not they have one until reveal
+        - specific size after reveal
+- Display "waiting for participants to join" if no participants
+- Display how to join session
+- A button for revealing participants' sizes
+- A button for resetting participants' sizes
+
+## Participant
+- Prompt for session key and name
+    - Both required
+    - Join button disabled until both provided
+- Explanation of session key
+- After join
+    - Display session and name
+    - Display input for sizing
+- Indicate when score has been reset
+
+# Feature priority
+1. Display participant names
+2. Display "waiting for participants to join" if no participants
+3. Prompt for session key and name, both required, and enable the join button when populated
+4. Display explanation of session key
