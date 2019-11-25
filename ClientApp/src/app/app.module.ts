d6eb91fbc64409f0efa7007f10ed2125b9c24a44
Timothy Wittig <iarejenius@gmail.com>
2019-11-25 18:06:03 -0500

    Remove unused template pieces

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -11,10 +11,6 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SessionComponent } from './session/session.component';
 import { ParticipantComponent } from './participant/participant.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,10 +18,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     SessionComponent,
     ParticipantComponent
   ],
