fb29b93d60a01a692e13f9fbda660dbdda737758
Timothy Wittig <iarejenius@gmail.com>
2019-11-26 10:38:45 -0500

    Get intitial angular tests passing

--- ClientApp/src/app/participant/participant.component.spec.ts
+++ ClientApp/src/app/participant/participant.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ParticipantComponent } from './participant.component';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatChipsModule, MatCardModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ParticipantComponent', () => {
   let component: ParticipantComponent;
@@ -8,9 +11,18 @@ describe('ParticipantComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ParticipantComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatChipsModule,
+        MatCardModule],
+      declarations: [ParticipantComponent]
+      // provide mocks of any dependencies to component
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- ClientApp/src/app/session/session.component.spec.ts
+++ ClientApp/src/app/session/session.component.spec.ts
@@ -1,16 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SessionComponent } from './session.component';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatChipsModule, MatCardModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
 
 describe('SessionComponent', () => {
   let component: SessionComponent;
   let fixture: ComponentFixture<SessionComponent>;
+  const activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', ['snapshot']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SessionComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatChipsModule,
+        MatCardModule],
+      declarations: [SessionComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteSpy }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
