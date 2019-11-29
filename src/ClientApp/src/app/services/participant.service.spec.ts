126d461ca5c4c8f1a6ac789b197bcd603fec4731
Timothy Wittig <iarejenius@gmail.com>
2019-11-29 17:30:29 -0500

    Create injectable signalr service

--- src/ClientApp/src/app/services/participant.service.spec.ts
+++ src/ClientApp/src/app/services/participant.service.spec.ts
@@ -1,15 +1,36 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ParticipantService } from './participant.service';
+import { SignalRConnection } from './signalr-connection.service';
 
 describe('ParticipantService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ParticipantService]
-    });
+
+  let sut: ParticipantService;
+
+  const signalrSpy = jasmine.createSpyObj('SignalRConnection', {
+    'on': () => Promise.resolve([]),
+    'off': () => Promise.resolve([]),
+    'send':() => Promise.resolve([])
   });
 
-  it('should be created', inject([ParticipantService], (service: ParticipantService) => {
-    expect(service).toBeTruthy();
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: SignalRConnection, useValue: signalrSpy }]
+    });
+
+    sut = TestBed.get(ParticipantService);
+  });
+
+  it('should be created', () => {
+    expect(sut).toBeTruthy();
+  });
+
+  it('should call send correctly when createParticipant is called', () => {
+    let testKey = "test key";
+    let testName = "test name";
+    sut.createParticipant(testKey, testName);
+    //expect(signalrSpy.send).toHaveBeenCalled();
+  });
+
 });
